1e8481389e835e985db5da7c0f39c7020aa1661f
Richard Mable <richardbmable@gmail.com>
2016-02-19 11:06:41 -0500

    profile created with Strava info when loggin in. Checked to make sure other user's could log in with Strava as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   def self.from_omniauth(auth)
+      # save this to the database, this will be the token that is sent to update a user's data
+      # probably should be in another table connected to the user that could be called StravaAuthData
+        puts "INFO IS #{auth.credentials.token.inspect}"
+
+    # this is saying where the user has provider Strava, and the matching UID, pull the first return or create it
+    # the provided block is only executed if running the create function
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     	user.email = auth.info.email
     	user.password = Devise.friendly_token[0,20]
+      puts "INFO IS #{auth.info.inspect}"
+     p = Profile.create(name: auth.info.name)
+     user.profile = p
   	end
    end
 
